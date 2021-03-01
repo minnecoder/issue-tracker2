43b1b0984683b71e8ffc30337f9360ff676fac7f
minnecoder <sdparkin@protonmail.com>
2021-03-01 15:37:37 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/api/v1/dashboard", dashboard)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client/build/index.html")))
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "./client/build/index.html")))
 }
 
 const PORT = process.env.PORT || 4000
