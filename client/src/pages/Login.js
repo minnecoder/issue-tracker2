8c60882cbe77fce41cf7b5f6488725ab9fd1c5d9
minnecoder <sdparkin@protonmail.com>
2021-03-04 21:55:36 -0600

    Removed Link import

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Login() {
