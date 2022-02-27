64338429ea2f8a64c8c209c328f9aca6ec71c744
minnecoder <sdparkin@protonmail.com>
2022-02-26 20:20:25 -0600

    changed export method

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
@@ -15,4 +15,3 @@ const connectDB = async () => {
     }
 };
 
-module.exports = connectDB;
